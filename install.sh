7c220283210ae05f2cf916fc1ae645ef1241eec0
Dewansh Nehra <dewanshnehra@gmail.com>
2024-01-19 01:41:08 +0530

    first commit

---
+++ install.sh
@@ -0,0 +1,121 @@
+#!/bin/bash
+
+if [ "$EUID" -ne 0 ]
+  then echo "
+  ############################################################################# 
+  ##         This script must be run as root or with sudo command            ##   
+  ##  before running the script switch to root user using <su> or <sudo su>  ##
+  #############################################################################
+  "
+  sleep 15
+  exit
+fi
+
+echo -e "
+ ██████╗ ███████╗██╗    ██╗ █████╗ ███╗   ██╗███████╗   ███╗   ██╗███████╗██╗  ██╗██████╗  █████╗ 
+ ██╔══██╗██╔════╝██║    ██║██╔══██╗████╗  ██║██╔════╝   ████╗  ██║██╔════╝██║  ██║██╔══██╗██╔══██╗ 
+ ██║  ██║█████╗  ██║ █╗ ██║███████║██╔██╗ ██║███████╗   ██╔██╗ ██║█████╗  ███████║██████╔╝███████║ 
+ ██║  ██║██╔══╝  ██║███╗██║██╔══██║██║╚██╗██║╚════██║   ██║╚██╗██║██╔══╝  ██╔══██║██╔══██╗██╔══██║
+ ██████╔╝███████╗╚███╔███╔╝██║  ██║██║ ╚████║███████║   ██║ ╚████║███████╗██║  ██║██║  ██║██║  ██║ 
+ ╚═════╝ ╚══════╝ ╚══╝╚══╝ ╚═╝  ╚═╝╚═╝  ╚═══╝╚══════╝   ╚═╝  ╚═══╝╚══════╝╚═╝  ╚═╝╚═╝  ╚═╝╚═╝  ╚═╝  
+ "
+echo -e "
+###################################################################################
+####           This script is written by Dewans Nehra.                        ####
+####           You can contact me at https://dewansnehra.xyz                  ####
+####           This script is written for Ubuntu 22.04                        ####
+####           This script will install Cloudstack 4.18                       ####
+###################################################################################
+"
+
+apt update && apt upgrade -y
+
+
+GATEWAY=$(ip r | awk '/default/ {print $3}')
+IP=$(ip r | awk '/src/ {print $9}')
+
+
+IP=$(ip r | awk '/src/ {print $9}')
+echo -e "127.0.0.1\tlocalhost\n$IP\tdevil.dewansnehra.xyz\tdevil" | sudo tee /etc/hosts
+
+
+cp /etc/netplan/01-network-manager-all.yaml /etc/netplan/01-network-manager-all.yaml.bak
+
+
+echo "network:
+    ethernets:
+        enp0s3:
+            dhcp4: false
+            addresses: [$IP/24]
+            gateway4: $GATEWAY
+            nameservers:
+                addresses: [8.8.8.8, 8.8.4.4]
+    version: 2" | sudo tee /etc/netplan/01-network-manager-all.yaml
+
+
+netplan apply
+netplan apply
+
+apt-get install -y openntpd openssh-server sudo vim htop tar intel-microcode bridge-utils mysql-server
+
+echo deb [arch=amd64] http://download.cloudstack.org/ubuntu jammy 4.18  > /etc/apt/sources.list.d/cloudstack.list
+
+wget -O - http://download.cloudstack.org/release.asc|gpg --dearmor > cloudstack-archive-keyring.gpg
+
+mv cloudstack-archive-keyring.gpg /etc/apt/trusted.gpg.d/
+
+apt update && apt upgrade -y
+apt-get install -y cloudstack-management cloudstack-usage
+
+
+echo -e "\nserver_id = 1\nsql-mode=\"STRICT_TRANS_TABLES,NO_ENGINE_SUBSTITUTION,ERROR_FOR_DIVISION_BY_ZERO,NO_ZERO_DATE,NO_ZERO_IN_DATE,NO_ENGINE_SUBSTITUTION\"\ninnodb_rollback_on_timeout=1\ninnodb_lock_wait_timeout=600\nmax_connections=1000\nlog-bin=mysql-bin\nbinlog-format = 'ROW'" | sudo tee -a /etc/mysql/mysql.conf.d/mysqld.cnf
+
+
+echo -e "[mysqld]" | sudo tee /etc/mysql/mysql.conf.d/cloudstack.cnf
+
+systemctl restart mysql
+
+echo "
+###################################################################################
+# In the next command if it will ask for password just press enter and do nothing #
+###################################################################################
+"
+
+mysql -u root -p -e "
+SELECT user,authentication_string,plugin,host FROM mysql.user;
+ALTER USER 'root'@'localhost' IDENTIFIED WITH mysql_native_password BY 'dewansnehra';
+use mysql;
+UPDATE user SET plugin='mysql_native_password' WHERE User='root';
+flush privileges;
+"
+apt-get install -y cloudstack-management cloudstack-usage
+cloudstack-setup-databases devil:devil@localhost --deploy-as=root:dewansnehra
+
+
+cloudstack-setup-management
+
+echo "
+###################################################################################
+####           Thank you for using this script.                                ####
+####       Dewans Nehra -  https://dewansnehra.xyz                             #### 
+###################################################################################
+"
+width=$(tput cols)
+progress_width=$((width - 20))
+sleep_duration=$(echo "60 / $progress_width" | bc -l)
+echo -n "Progress: ["
+for i in $(seq 1 $progress_width)
+do
+    sleep $sleep_duration
+    echo -n "#"
+done
+echo "]"
+
+
+echo "
+###################################################################################
+####           Installation done. You can go to http://localhost:8080          ####
+####           to access the pannel.                                           ####
+####           Dewans Nehra -  https://dewansnehra.xyz                         ####
+###################################################################################
+"
---
+++ readme.md
@@ -0,0 +1,50 @@
+# Cloudstack 4.18 Installation Script for Ubuntu 22.04
+
+This script automates the installation of Cloudstack 4.18 on Ubuntu 22.04. It was written by Dewans Nehra. You can contact me at [https://dewansnehra.xyz](https://dewansnehra.xyz).
+
+## Usage
+
+1. Download the script using git clone command:
+```bash
+    git clone <https://github.com/DewanshNehra/Cloudstack-Installation>
+```
+2. Change the directory to that folder:
+```bash
+   cd cloudstack-installation
+```
+3. Make the script executable with the command: 
+```bash
+chmod +x install.sh
+```
+4. Switch to root before running the script and enter your root password:
+```bash
+su
+```
+or
+```bash
+sudo su
+``` 
+5. Run the script using the command:
+```bash
+./install.sh
+```
+## What the script does
+
+The script performs the following steps:
+
+1. Checks if it is run as root or with sudo.
+2. Updates and upgrades the system packages.
+3. Configures the network settings.
+4. Installs necessary packages including openntpd, openssh-server, sudo, vim, htop, tar, intel-microcode, bridge-utils, mysql-server, and Cloudstack.
+5. Configures MySQL.
+6. Sets up Cloudstack databases and management.
+
+## Note
+
+During the MySQL configuration, if it asks for a password, just press enter and do nothing.
+
+## After Installation
+
+Once the installation is done, you can access the Cloudstack panel at [http://localhost:8080](http://localhost:8080).\
+Username: `admin`\
+Password: `password`
